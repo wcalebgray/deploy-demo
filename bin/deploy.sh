7de6e4cb3c51e219a8a533f5a8544b7dcb87b339
Caleb Gray <caleb.gray@augustineinstitute.org>
2017-08-28 19:03:05 -0500

    init(): initial commit of deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Deploys by using the following:
+# - check which branch I'm on
+# - build docker image
+# - tag docker image
+# - push docker image
+# - create new task definition revision
+# - Update service
+
+APP="deploy-demo"
+REPO="461781854431.dkr.ecr.us-west-2.amazonaws.com/$APP"
+LOCAL_REPO="$APP"
+TASK="$APP"
+cluster="default"
+
+# Check which branch I'm on and set environment based on branch
+if [ -z "$GIT_BRANCH" ]; then
+  branch=`git rev-parse --abbrev-ref HEAD`
+else
+  branch=`echo "$GIT_BRANCH" | awk -F/ '{ print $NF }'`
+fi
+
+if [ "$branch" = "production" ]; then
+  env="production"
+else
+  env="stage"
+fi
+
+# build docker
+echo "Building docker image for $env"
+docker build -t "$LOCAL_REPO" .
+
+# determine primary tag
+tag=`git describe --abbrev=0`
+build_number=`git rev-list "$tag".. --count`
+if [ "$build_number" = "0" ]; then
+  tag=`semver "$tag"`
+else
+  tag=`semver "$tag" -i prerelease`".$build_number"
+fi
+
+# login to docker repository
+echo
+echo "Logging in to AWS docker"
+`aws ecr get-login | sed 's/-e none//'`
+
+# push docker tags
+echo
+echo "Pushing docker tag: $tag"
+image="$REPO:$tag"
+docker tag "$LOCAL_REPO:latest" "$image"
+docker push "$image"
+
+echo
+echo "Updating task: $TASK"
+
+# pull current task definition and replace the image(s)
+current_def=`aws ecs describe-task-definition --task-def "$TASK"`
+current_containers=`node -pe "JSON.stringify(JSON.parse(process.argv[1]).taskDefinition.containerDefinitions)" "$current_def"`
+new_containers=`echo "$current_containers" | sed "s~$REPO:[A-Za-z0-9._-]*~$image~g"`
+
+# create a new revision of the task family
+aws ecs register-task-definition --family $TASK --container-definitions "$new_containers" > /dev/null
+
+echo "Updating $TASK service of cluster: $cluster"
+aws ecs update-service --cluster $cluster --service $TASK --task-definition $TASK > /dev/null
+
+echo 'Finished'
